2ac60d160bea0f6b1d4dcce4c6747111a4fb8e47
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-07 09:28:24 +0000

    commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#ejercicio 1
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1[-1])
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1[1])
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1[0])
+print("")
+tupla= ("Sardinera", "Portitxol", "Granadella")
+print(tupla[-1])
+tupla= ("Sardinera", "Portitxol", "Granadella")
+print(tupla[0])
+tupla= ("Sardinera", "Portitxol", "Granadella")
+print(tupla[1])
+print("")
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1)
+print("")
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1[0])
+print("")
+lista2= ["Sardinera", "Barraca", "Grandalla"]
+print(lista2[-1])
+lista2= ["Sardinera", "Barraca", "Grandalla"]
+print(lista2[0])
+lista2= ["Sardinera", "Barraca", "Grandalla"]
+print(lista2[1])
+print("")
+tupla= ("Sardinera", "Portitxol", "Granadella")
+print(tupla[2])
+print("")
+len(lista1)
+print(len(lista1))
+len(tupla)
+print(len(tupla))
+print("")
+lista1= ["Sardinera", "Portitxol", "Granadella"]
+print(lista1.index("Portitxol"))
+tupla= ("Sardinera", "Portitxol", "Granadella")
+print(tupla.index("Sardinera"))
+print("")
+lista1= ["Sardinera", "Portitxol", "Grandalla"] 
+lista3=["Caleta"]
+print(lista1 + lista3)
+print("")
+lista1=["Sardinera", "Portitxol", "Grandalla"] 
+lista1.pop(2)
+print(lista1)
+
+#ejrecicio 2
+dicc= dict(("Sardinera", "Portitxol", "Granadella"))
+print(dicc["Sardinera", "Portitxol", "Granadella"])
